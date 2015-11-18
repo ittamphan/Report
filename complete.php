4dd41b352cab93ead62f7c0386079e6cc6c96516
giapluuspkt <giapluuspkt@gmail.com>
2015-11-18 09:15:33 +0700

    nganluong

---
+++ complete.php
@@ -0,0 +1,5 @@
+<?php
+	error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+	$html .="<div align=\"center\">Cám ơn quý khách, quá trình thanh toán đã được hoàn tất. Chúng tôi sẽ kiểm tra và chuyển hàng sớm!</div>";
+	echo $html;
+?>
